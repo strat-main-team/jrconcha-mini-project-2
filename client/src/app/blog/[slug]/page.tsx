147637328325eaf0cf42b59bd6f3c000a3604e39
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-07-12 21:00:55 +0800

    Fix: Attempt fix by destructuring and typing slug, and using npx canary

--- client/src/app/blog/[slug]/page.tsx
+++ client/src/app/blog/[slug]/page.tsx
@@ -7,8 +7,8 @@ import BlogCommentBlock from "@/components/blog-comment";
 
 type Params = Promise<{ slug: string }>;
 export default async function BlogPostPage({ params }: { params: Params }) {
-  const { slug } = await params;
-  const id = Number(slug.split("-")[0]);
+  const { slug }: {slug: string} = await params; // https://stackoverflow.com/questions/79124951/type-error-in-next-js-route-type-params-id-string-does-not-satis
+  const id = Number(slug.split("-")[0]);  
   const blogResult = await getBlogPostById(id);
   const commentResult = await getCommentsByPostID(id);
   if (!blogResult.success) return notFound(); // Type Guard
